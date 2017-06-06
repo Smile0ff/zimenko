ba13cc355f3b514138c6142e91f6de87f18c714d
Smile0ff <a.parneta@gmail.com>
2017-06-06 19:09:08 +0300

    blog

--- assets/js/containers/blogContainer.js
+++ assets/js/containers/blogContainer.js
@@ -1,9 +1,11 @@
 import Menu from '@components/menu';
+import Gallery from '@components/gallery';
 
 class BlogContainer{
 
     constructor(){
         this.menu = new Menu();
+        this.gallery = new Gallery();
     }
 
 }
