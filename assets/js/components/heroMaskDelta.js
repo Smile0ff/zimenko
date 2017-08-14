1e3ac994dd59b60e6205589cdd2989b528a9285b
Smile0ff <a.parneta@gmail.com>
2017-08-14 17:35:56 +0300

    mask fix

--- assets/js/components/heroMaskDelta.js
+++ assets/js/components/heroMaskDelta.js
@@ -4,6 +4,8 @@ const titleText = document.querySelector('#loader-holder .title-text');
 const DELTA = -90;
 
 const heroMaskDelta = () => {
+    if(window.innerWidth >= 600) return;
+    
     maskText.setAttribute('dy', DELTA);
     titleText.setAttribute('dy', DELTA);
 }
